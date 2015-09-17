87e30a46bb77bfbaa557e4d8b1ade937b2d60828
13philistine <poltava-0706@mail.ru>
2015-09-18 02:02:46 +0600

    44

--- dichotomy/main.py
+++ dichotomy/main.py
@@ -1,4 +1,4 @@
-# coding: cp866
+п»ї# coding: cp866
 e = float(1e-5)
 a = 0
 b = 16
@@ -6,13 +6,13 @@ b = 16
 def f(x):
     return (pow(x,3)) - 13
 
-print("Посчитать к-во шагов, когда на отрезке будет E")
+print("РџРѕСЃС‡РёС‚Р°С‚СЊ Рє-РІРѕ С€Р°РіРѕРІ, РєРѕРіРґР° РЅР° РѕС‚СЂРµР·РєРµ Р±СѓРґРµС‚ E")
 print("E=", e)
 #x^5=1613
 fout = open('output.txt', 'w')
-print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  Итого=", (a+b)/2, file=fout )
+print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  РС‚РѕРіРѕ=", (a+b)/2, file=fout )
 print("f(a)=", f(a), "  f(middle)=", f((a+b)/2), "\n", file=fout)
-print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  Итого=", (a+b)/2)
+print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  РС‚РѕРіРѕ=", (a+b)/2)
 print("f(a)=", f(a), "  f(middle)=", f((a+b)/2))
 while b - a > e:
     c = (a + b) / 2
@@ -20,8 +20,8 @@ while b - a > e:
         b = c
     else:
         a = c
-    print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  Итого=", (a+b)/2, file=fout )
+    print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  РС‚РѕРіРѕ=", (a+b)/2, file=fout )
     print("f(a)=", f(a), "  f(middle)=", f((a+b)/2),"\n", file=fout)
-    print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  Итого=", (a+b)/2)
+    print("a= ", a,"  b= ", b, "  (b-a)= ", (b-a), "  РС‚РѕРіРѕ=", (a+b)/2)
     print("f(a)=", f(a), "  f(middle)=", f((a+b)/2),"\n")
 fout.close()
